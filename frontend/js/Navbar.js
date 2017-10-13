a9774009f0dae3bca52227fc94d8b80f29ae691a
Thanos Paravantis <p16112@students.cs.unipi.gr>
2017-10-13 14:49:20 +0300

    Add comments to the navbar object

--- frontend/js/Navbar.js
+++ frontend/js/Navbar.js
@@ -1,14 +1,21 @@
 var Navbar = {
+    // Default id's for navbar togglers
     registerToggleId: 'toggle-register',
     loginToggleId: 'toggle-login',
+
+    // The name of the active class in the navbar
     activeClass: 'active',
+
+    // The name of the item list class in the navbar
     navbarItemsClass: 'items',
 
+    // Registers all toggle events
     init: function() {
         $("#" + Navbar.registerToggleId).click(Navbar.togglePanel);
         $("#" + Navbar.loginToggleId).click(Navbar.togglePanel);
     },
 
+    // Toggles the appropiate panel when a navbar item is clicked
     togglePanel: function(event) {
         clicked = $(event.target);
         panelId = clicked.attr('id').replace('toggle-', '');
@@ -20,12 +27,14 @@ var Navbar = {
         panel.show();
     },
 
+    // Hides all active items in the navbar
     hideToggled: function(items) {
         $('.' + Navbar.navbarItemsClass).find('.' + Navbar.activeClass).each(function(index, item) {
             $(item).removeClass(Navbar.activeClass);
         });
     },
 
+    // Hides all visible panel forms
     hideForms: function() {
         $('.form').each(function(index, item) {
             $(item).hide();
