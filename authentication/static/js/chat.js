b6e550bbd60f6dbb968866eb30cc14a8fff36d88
johnythefoxGR <john.al.vra@hotmail.com>
2017-10-16 14:39:43 +0000

    -fixed message input

--- authentication/static/js/chat.js
+++ authentication/static/js/chat.js
@@ -11,33 +11,28 @@ function start(chatname='room_one'){
                 <div class="panel chat" id="'+chatname+'">\n\
                 </div>\n\
                 <div class="message">\n\
-                 <input type="text" name="message" id="message'+chatname+'" value=""/>\n\
+                 <input type="text" name="message" id="message'+chatname+'" value=""\
+                 onkeydown="if (event.keyCode == 13) { send(); return false; }"/>\n\
                  <button type="button" id="button'+chatname+'" onclick="send()">Send</button>\n\
                 </div>\n\
             </section>\n\
         ';
         document.getElementById("dashboard").innerHTML += chatwindow
-        document.getElementById("message"+chatname).addEventListener("keydown", function(event) {
-            event.preventDefault();
-            if (event.keyCode == 13) {
-                document.getElementById("button"+chatname).click();
-                document.getElementById("message"+chatname).value=""
-            }
-        });
         socket.push( new WebSocket("ws://" + window.location.host + "/"+ chatname + "/"));
         socket[chatlist.indexOf(chatname)].onmessage = function(e) {
-           var data = JSON.parse(e.data)
+           var data = JSON.parse(e.data);
            document.getElementById(chatname).innerHTML +='<p> '+ data.username + ': </p> <p>'+ data.text + '</p>';
         }
     }
 }
 function send(chatname='room_one') {
-   var message = document.getElementById("message"+chatname).value
+   var message = document.getElementById("message"+chatname).value;
+   document.getElementById("message"+chatname).value="";
    try{
-      socket[chatlist.indexOf(chatname)].send(message)
+      socket[chatlist.indexOf(chatname)].send(message);
    }catch(err){
       socket[chatlist.indexOf(chatname)].onopen = function() {
-        socket[chatlist.indexOf(chatname)].send(message)
+        socket[chatlist.indexOf(chatname)].send(message);
       }
    }
 }
